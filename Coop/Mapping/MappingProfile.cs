225dcd636f101da0d8add7ccbf1b3678fbabf7f3
Ken <okalang.ds@gmail.com>
2021-07-13 11:33:01 +0200

    Modified AddAutoMapper

--- Coop/Mapping/MappingProfile.cs
+++ Coop/Mapping/MappingProfile.cs
@@ -1,10 +1,6 @@
 ﻿using AutoMapper;
 using Coop.Controllers.Resources;
 using Coop.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Coop.Mapping
 {
--- Coop/Startup.cs
+++ Coop/Startup.cs
@@ -23,6 +23,8 @@ namespace Coop
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddAutoMapper(typeof(Startup));
             services.AddDbContext<CoopDbContext>(options =>
             {
                 if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
