e05d1480698e189fc4b730485766a902140c509c
Ken <okalang.ds@gmail.com>
2021-07-13 15:23:11 +0200

    created a produce component

---
+++ Coop/ClientApp/src/app/produce-form/produceform/produceform.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-produceform',
+  moduleId: module.id,
+  templateUrl: 'produceform.component.html',
+  styleUrls: ['produceform.component.css']
+})
+export class ProduceformComponent {
+  constructor() {
+  }
+}
