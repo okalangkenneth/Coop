4057f83ce1d10b52a6c7dafab452a0d44e3fed90
Ken <okalang.ds@gmail.com>
2021-07-18 08:09:18 +0200

    created Core folder and moved models into it

--- Coop/Controllers/ProduceTypesController.cs
+++ Coop/Controllers/ProduceTypesController.cs
@@ -14,13 +14,16 @@ namespace Coop.Controllers
         private readonly CoopDbContext context;
         private readonly IMapper mapper;
 
-        public ProduceTypesController(CoopDbContext context,IMapper mapper)
+        public ProduceTypesController(CoopDbContext context, IMapper mapper)
         {
             this.context = context;
             this.mapper = mapper;
         }
         [HttpGet("/api/produceTypes")]
-        public async Task <IEnumerable<ProduceTypeResource>>GetProduceTypes()
+        public async Task<IEnumerable<ProduceTypeResource>> GetProduceTypes()
         {
-            var produceTypes = await context.ProduceTypes.Include(m=> m.ProduceNames).ToListAsync();
-            return mapper.Map<List<ProduceType>, List<ProduceTypeResource>>((List<ProduceType>)prod
+            var produceTypes = await context.ProduceTypes.Include(m => m.ProduceNames).ToListAsync();
+            return mapper.Map<List<ProduceType>, List<ProduceTypeResource>>(produceTypes);
+        }
+    }
+}
